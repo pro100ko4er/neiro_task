b2ee8fd187fdd4d4b629bcb6ae784627c68e1f45
pro100ko4er <ai329390@gmail.com>
2024-12-09 07:32:22 +0300

    fixes

--- src/db/scheme.sql
+++ src/db/scheme.sql
@@ -8,16 +8,6 @@ create TABLE user(
     role VARCHAR(255)
 );
 
-
-create TABLE messages(
-    id SERIAL PRIMARY KEY,
-    text VARCHAR,
-    time VARCHAR(255),
-    type VARCHAR(255),
-    user_id INTEGER,
-    FOREIGN KEY (user_id) REFERENCES user (id)
-)
-
 create TABLE token(
     id SERIAL PRIMARY KEY,
     user_id INTEGER,
